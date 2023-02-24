c118438fbd1e5b8d86bd581cac79fe73912f0d7c
ivix <ivixmail@gmail.com>
2023-02-24 23:16:16 +0300

    Add lock for books db

--- src/Repository/Book/JsonBookRepository.php
+++ src/Repository/Book/JsonBookRepository.php
@@ -5,25 +5,38 @@ namespace App\Repository\Book;
 
 use App\Entity\Book;
 use App\Repository\BookRepositoryInterface;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\LockInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class JsonBookRepository implements BookRepositoryInterface
 {
+    private ?LockInterface $lock = null;
 
-    private $jsonFilePath;
-
-    /**
-     * @var Book[]
-     */
-    private ?array $books = null;
-
-    private SerializerInterface $serializer;
-
-    public function __construct(SerializerInterface $serializer, $booksJsonFilePath)
+    public function __construct(
+        private readonly SerializerInterface $serializer,
+        private readonly string              $booksJsonFilePath,
+        private readonly LockFactory         $lockFactory
+    )
     {
-        $this->serializer = $serializer;
-        $this->jsonFilePath = $booksJsonFilePath;
+    }
 
+    private function lock()
+    {
+        if (!$this->lock) {
+            $this->lock = $this->lockFactory->createLock('books.db');
+        }
+
+        $this->lock->acquire(true);
+    }
+
+    private function unlock()
+    {
+        if (!$this->lock) {
+            throw new \RuntimeException('Lock is null');
+        }
+
+        $this->lock->release();
     }
 
     /**
@@ -31,33 +44,31 @@ class JsonBookRepository implements BookRepositoryInterface
      */
     public function getBooks(): array
     {
-        if ($this->books === null) {
-            $this->books = $this->loadBooks();
+        $this->lock();
+
+        try {
+            return $this->loadBooks();
+        } finally {
+            $this->unlock();
         }
 
-        return $this->books;
     }
 
-    private function setBooks(array $books)
-    {
-        $this->books = $books;
-    }
-
-    private function flushBooks()
+    private function flushBooks(array $books)
     {
         $strings = [];
-        foreach ($this->books as $id => $book) {
+        foreach ($books as $id => $book) {
             $strings[$id] = $this->serializer->serialize($book, 'json');
         }
 
-        file_put_contents($this->jsonFilePath, serialize($strings));
+        file_put_contents($this->booksJsonFilePath, serialize($strings));
     }
 
-    private function loadBooks()
+    private function loadBooks(): array
     {
         $books = [];
-        if (file_exists($this->jsonFilePath)) {
-            $strings = unserialize(file_get_contents($this->jsonFilePath));
+        if (file_exists($this->booksJsonFilePath)) {
+            $strings = unserialize(file_get_contents($this->booksJsonFilePath));
             foreach ($strings as $id => $string) {
                 $books[$id] = $this->serializer->deserialize($string, Book::class, 'json');
             }
@@ -67,30 +78,45 @@ class JsonBookRepository implements BookRepositoryInterface
 
     public function saveBook(Book $book)
     {
-        $books = $this->getBooks();
-        $books[$book->getId()] = $book;
-        $this->setBooks($books);
-        $this->flushBooks();
+        $this->lock();
+
+        try {
+            $books = $this->loadBooks();
+            $books[$book->getId()] = $book;
+            $this->flushBooks($books);
+        } finally {
+            $this->unlock();
+        }
     }
 
     public function getBook(string $id): Book
     {
-        $books = $this->getBooks();
-        if (!isset($books[$id])) {
-            throw BookNotFoundException::byId($id);
-        }
+        $this->lock();
 
-        return $books[$id];
+        try {
+            $books = $this->loadBooks();
+            if (!isset($books[$id])) {
+                throw BookNotFoundException::byId($id);
+            }
+
+            return $books[$id];
+        } finally {
+            $this->unlock();
+        }
 
     }
 
     public function removeBook(string $id)
     {
-        $books = $this->getBooks();
-        if (isset($books[$id])) {
-            unset($books[$id]);
-            $this->setBooks($books);
-            $this->flushBooks();
+        $this->lock();
+        try {
+            $books = $this->loadBooks();
+            if (isset($books[$id])) {
+                unset($books[$id]);
+                $this->flushBooks($books);
+            }
+        } finally {
+            $this->unlock();
         }
     }
 }
